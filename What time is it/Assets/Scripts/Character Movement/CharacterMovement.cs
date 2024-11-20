b7660aa19212c88bb59147ce6cd31b34ddf05326
PabloElBebe <rinat_aminov_2006@mail.ru>
2024-11-20 23:03:46 +0300

    Update (What time is it?)

--- What time is it/Assets/Scripts/Character Movement/CharacterMovement.cs
+++ What time is it/Assets/Scripts/Character Movement/CharacterMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
@@ -5,12 +6,15 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private float _moveSpeedField;
     [SerializeField] private float _jumpForceField;
     [SerializeField] private LayerMask _layerMask;
-    
+
     private Rigidbody2D _rb2D;
     private Animator _animator;
 
     private float _horizontalInput;
-    
+
+    private float _velocityX;
+    private float _velocityY;
+
     private float _moveSpeed => _moveSpeedField;
     private float _jumpForce => _jumpForceField;
 
@@ -46,14 +50,14 @@ public class CharacterMovement : MonoBehaviour
 
         if (_horizontalInput < 0)
             rotateAngle = 180;
-        
+
         transform.rotation = Quaternion.Euler(rotation.x, rotateAngle, rotation.z);
     }
-    
+
     private void Jump()
     {
         RaycastHit2D hit2D = Physics2D.Raycast(transform.position, Vector2.down, 4.6f, _layerMask);
-        
+
         if (_rb2D == null || !hit2D)
             return;
 
@@ -65,10 +69,17 @@ public class CharacterMovement : MonoBehaviour
         if (_animator == null || _rb2D == null)
             return;
         
-        _animator.SetFloat("x", _rb2D.velocity.x);
-        _animator.SetFloat("y", _rb2D.velocity.y);
+        if (Mathf.Abs(_velocityY - _rb2D.velocity.normalized.y) > 0.005f)
+            _velocityY = Mathf.Lerp(_velocityY, _rb2D.velocity.normalized.y, Time.deltaTime * 12);
+
+        if (Mathf.Abs(_velocityX - _rb2D.velocity.normalized.x) > 0.005f)
+            _velocityX = Mathf.Lerp(_velocityX, _rb2D.velocity.normalized.x, Time.deltaTime * _moveSpeed);
+
+        _animator.SetFloat("x", _velocityX);
+        _animator.SetFloat("y", _velocityY);
+
     }
-    
+
     private void Inputs()
     {
         _horizontalInput = Input.GetAxis("Horizontal");
